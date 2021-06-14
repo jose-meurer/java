fc140bffba49d3dbdc6f4b468a63a7b8b4d62945
Jose Meurer <josewm18@gmail.com>
2021-06-14 19:08:56 -0300

    Solucao muito ruim

--- tratamento_excecoes/aula1 - muito_ruim/src/model/entities/Reservation.java
+++ tratamento_excecoes/aula1 - muito_ruim/src/model/entities/Reservation.java
@@ -42,8 +42,13 @@ public class Reservation {
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
+    public void updateDates(Date checkIn, Date checkOut) {
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
     public String toString() {
-        return "Room"
+        return "Room "
                 + roomNumber
                 + ", check-in: "
                 + sdf.format(checkIn)
