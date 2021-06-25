73e1c458f1861e37eb80609fc4211b506d36578e
Jose Meurer <josewm18@gmail.com>
2021-06-25 12:07:57 -0300

    FileWriter and BufferedWriter

---
+++ trabalhando_com_arquivos/aula/src/application/Program4.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program4 {
+    public static void main(String[] args) {
+        // FileWriter and BufferedWriter
+        String[] lines = new String[] {
+                "Good morning",
+                "Good afternoon",
+                "Good evening"
+        };
+
+        String path = "D:\\workspace\\Projetos\\Curso_Udemy_Java_Nelio_Alves\\trabalhando_com_arquivos\\aula\\out.txt";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+            // Apenas path, cria ou recria o arquivo || Colocando path, true, acrescenta no final do arquivo.
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
