43b001c2fc2a39c57675bee55fc7ea08ebc8b41f
Jose Meurer <josewm18@gmail.com>
2021-06-25 23:43:54 -0300

    Solucao professor com interface

--- interfaces/aula/src/model/services/BrazilTaxService.java
+++ interfaces/aula/src/model/services/BrazilTaxService.java
@@ -1,8 +1,9 @@
 package model.services;
 
-public class BrazilTaxService {
+public class BrazilTaxService implements TaxService {
 
-    public double tax(double amount) {
+    @Override
+    public Double tax(Double amount) {
         if (amount <= 100.0) {
             return amount * 0.20;
         }else {
