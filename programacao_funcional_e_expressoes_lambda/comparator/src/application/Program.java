3ff124c3b28142c5538bac33bc4f5f48f25ad40b
Jose Meurer <josewm18@gmail.com>
2021-07-14 09:42:48 -0300

    Comparator objeto de classe anonima

--- programacao_funcional_e_expressoes_lambda/comparator/src/application/Program.java
+++ programacao_funcional_e_expressoes_lambda/comparator/src/application/Program.java
@@ -4,6 +4,7 @@ import entities.MyComparator;
 import entities.Product;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Program {
@@ -16,7 +17,14 @@ public class Program {
         list.add(new Product("Tablet", 450.0));
         list.add(new Product("Cellphone", 390.0));
 
-        list.sort(new MyComparator());
+        Comparator<Product> comp = new Comparator<Product>() {
+            @Override
+            public int compare(Product p1, Product p2) {
+                return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+            }
+        };
+
+        list.sort(comp);
 
         for (Product prod : list) {
             System.out.println(prod);
