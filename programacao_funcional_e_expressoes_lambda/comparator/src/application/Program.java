ea338fee41e8b26cf4801f1ba5f84bc4b1dec995
Jose Meurer <josewm18@gmail.com>
2021-07-14 10:27:28 -0300

    Comparator objeto de expressao lambda sem chaves

--- programacao_funcional_e_expressoes_lambda/comparator/src/application/Program.java
+++ programacao_funcional_e_expressoes_lambda/comparator/src/application/Program.java
@@ -16,9 +16,7 @@ public class Program {
         list.add(new Product("Tablet", 450.0));
         list.add(new Product("Cellphone", 390.0));
 
-        Comparator<Product> comp = (p1,p2) -> {
-            return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-        };
+        Comparator<Product> comp = (p1,p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 
         list.sort(comp);
 
