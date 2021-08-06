f5fc450d65775550271aafd121e6396e6956de98
Jose Meurer <josewm18@gmail.com>
2021-08-06 20:14:56 -0300

    Lambda declared

--- programacao_funcional_e_interface_funcional/consumer/src/application/Program.java
+++ programacao_funcional_e_interface_funcional/consumer/src/application/Program.java
@@ -6,6 +6,7 @@ import util.PriceUpdate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 public class Program {
 
@@ -20,6 +21,9 @@ public class Program {
 		list.add(new Product("Tablet", 350.0));
 		list.add(new Product("HD Case", 80.90));
 		list.add(new Product("Cellphone", 287.0));
+
+		double factor = 1.1;
+		Consumer<Product> cons = p -> p.setPrice(p.getPrice() * factor);
 
 		list.forEach(Product::priceUpdate);
 
