85c1be7b6768742b26f5a4b4f25f836d0b02495d
Jose Meurer <josewm18@gmail.com>
2021-08-16 17:11:20 -0300

    Pipeline

--- programacao_funcional_e_expressoes_lambda/pipeline/src/application/Program.java
+++ programacao_funcional_e_expressoes_lambda/pipeline/src/application/Program.java
@@ -1,10 +1,30 @@
 package application;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Program {
 
 	public static void main(String[] args) {
-		
-		
+
+		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
+
+		Stream<Integer> st1 = list.stream().map(x -> x * 10);
+		System.out.println(Arrays.toString(st1.toArray()));
+
+		int sum = list.stream().reduce(0, (x, y) -> x + y);
+		int sum1 = list.stream().filter(x -> x % 2 == 0).reduce(0, (x,y) -> x + y);
+		int sum2 = list.stream().mapToInt(x -> x).sum();
+		System.out.println("Sum = " + sum);
+		System.out.println("Sum = " + sum1);
+		System.out.println("Sum = " + sum2);
+
+		List<Integer> newList = list.stream().filter(x -> x % 2 == 0)
+				.map(x -> x * 10).collect(Collectors.toList());
+		System.out.println(Arrays.toString(newList.toArray()));
+
 		
 		}
 }
